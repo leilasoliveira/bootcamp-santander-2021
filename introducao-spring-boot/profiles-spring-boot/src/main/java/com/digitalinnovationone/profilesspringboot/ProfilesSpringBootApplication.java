d8f6655a2f098298e239b1d186de4af6db054df6
Leila Farias <leila.s.oliveira@hotmail.com>
2021-07-31 19:01:27 -0300

    Mais projetos e notas de aula até Intro Spring Boot

---
+++ introducao-spring-boot/profiles-spring-boot/src/main/java/com/digitalinnovationone/profilesspringboot/ProfilesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.profilesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesSpringBootApplication.class, args);
+	}
+
+}
---
+++ introducao-spring-boot/profiles-spring-boot/src/test/java/com/digitalinnovationone/profilesspringboot/ProfilesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.profilesspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilesSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
